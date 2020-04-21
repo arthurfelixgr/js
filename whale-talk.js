109ba465f427a0d90b748480ba7245f4399ac893
arthurfelixgr <arthurfelixgr@gmail.com>
2020-04-21 16:50:11 -0300

    Create whale-talk.js

---
+++ whale-talk.js
@@ -0,0 +1,25 @@
+let input = "The ocean is a desert with its life underground and a perfect disguise above";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if ( input[i] === vowels[j] ) {
+      if (vowels[j] === "e" || vowels[j] === "u") {
+        resultArray.push( input[i] );
+        resultArray.push( input[i] );
+      }
+/*
+      if(input[i] === "e") {
+        resultArray.push('ee');
+      }
+      else if (input[i] === "u") {
+        resultArray.push("uu");
+      }
+*/
+      else {
+        resultArray.push( input[i] );
+      }
+    }
+  }
+}
+console.log( resultArray.join('').toUpperCase() );
